d66273b1741a736a89ff2caf7d548232528c1a4d
wanghaichao <m18888106873@163.com>
2020-10-21 23:22:14 +0800

    添加测试用例

--- src/test/java/demo/elastic/search/request/dsl/term/RegexpRequestTests.java
+++ src/test/java/demo/elastic/search/request/dsl/term/RegexpRequestTests.java
@@ -6,6 +6,7 @@ import demo.elastic.search.po.request.SearchSourceBuilder;
 import demo.elastic.search.po.request.aggs.VoidAggs;
 import demo.elastic.search.po.request.dsl.term.RegexpQuery;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -24,6 +25,25 @@ public class RegexpRequestTests {
         request.from(0).size(1).query(QueryBuilders.regexpQuery("city", "dant.*"));
         log.info("请求body:{}", request.getRequestBody());
         String response = searchService.DSL_search_regexp("index_bulk", request);
+        log.info("response:{}", response);
+    }
+
+    @Test
+    public void testRegexpRequest2() {
+        SearchSourceBuilder<RegexpQuery, VoidAggs> request = new SearchSourceBuilder<>();
+        request.from(0).size(1).query(QueryBuilders.regexpQuery("F1_0088", "[0-9]*"));
+        log.info("请求body:{}", request.getRequestBody());
+        String response = searchService.DSL_search_regexp("tb_object_0088", request);
+        log.info("response:{}", response);
+    }
+
+    @Test
+    public void testRegexpRequest3() {
+        SearchSourceBuilder<RegexpQuery, VoidAggs> request = new SearchSourceBuilder<>();
+        String regex = ".*";
+        request.from(0).size(1).query(QueryBuilders.regexpQuery("F1_0088", regex));
+        log.info("请求body:{}", request.getRequestBody());
+        String response = searchService.DSL_search_regexp("tb_object_0088", request);
         log.info("response:{}", response);
     }
 }
